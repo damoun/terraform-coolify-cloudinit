156d6e6b8bb9b8cc43c2329ac1539b7a90e4a5c7
Damien Plénard <damien@plenard.me>
2025-10-10 22:30:54 +0200

    fix: cleanup install script to reduce length

--- data.tf
+++ data.tf
@@ -2,6 +2,24 @@ data "http" "install_script" {
   url = var.url_install_script
 }
 
+locals {
+  install_script_lines = split("\n", data.http.install_script.response_body)
+  install_script_no_comments = [
+    for line in local.install_script_lines :
+    line if !startswith(trimspace(line), "#")
+  ]
+  install_script_no_inline_comments = [
+    for line in local.install_script_no_comments :
+    replace(line, "#.*$", "")
+  ]
+  install_script_minified = [
+    for line in local.install_script_no_inline_comments :
+    trimspace(replace(replace(line, "\t", " "), "  ", " "))
+    if trimspace(replace(replace(line, "\t", " "), "  ", " ")) != ""
+  ]
+  install_script_clean = join("\n", local.install_script_minified)
+}
+
 data "cloudinit_config" "coolify" {
   gzip          = false
   base64_encode = false
@@ -9,7 +27,6 @@ data "cloudinit_config" "coolify" {
   part {
     filename     = "install.sh"
     content_type = "text/x-shellscript"
-
-    content = data.http.install_script.response_body
+    content      = local.install_script_clean
   }
 }
