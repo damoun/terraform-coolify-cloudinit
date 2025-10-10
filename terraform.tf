eda9f7501e66f7aefac4130f89da9fe4b95b93e1
Damien Plénard <damien@plenard.me>
2025-10-10 19:53:49 +0200

    chore(deps): remove providers version constraint

--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,9 @@ terraform {
   required_providers {
     http = {
       source  = "hashicorp/http"
-      version = "3.4.4"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "2.3.7"
     }
   }
 }
