20b41104a3a0db6ee12808bbbc8baa0947d8eb65
Damien Plénard <damien@plenard.me>
2025-10-10 22:31:25 +0200

    test: remove hetzner provider from examples

--- examples/main.tf
+++ examples/main.tf
@@ -2,28 +2,12 @@
 # This code is found in the examples directory.
 terraform {
   required_version = ">= 1.8.4"
-  required_providers {
-    hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = "1.47.0"
-    }
-  }
 }
-
-provider "hcloud" {}
 
 module "coolify" {
   source = "../"
 }
 
-resource "hcloud_server" "coolify" {
-  name        = "coolify"
-  image       = "ubuntu-24.04"
-  server_type = "cax11"
-  location    = "nbg1"
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-  user_data = module.coolify.user_data
+output "user_data" {
+  value = module.coolify.user_data
 }
