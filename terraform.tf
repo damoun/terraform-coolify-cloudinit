2dbf5212aa19277124a7b95753c99debe5031c0a
Damien Plénard <damien@plenard.me>
2025-10-10 19:54:54 +0200

    style: terraform format

--- terraform.tf
+++ terraform.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.8.4"
   required_providers {
     http = {
-      source  = "hashicorp/http"
+      source = "hashicorp/http"
     }
     cloudinit = {
-      source  = "hashicorp/cloudinit"
+      source = "hashicorp/cloudinit"
     }
   }
 }
